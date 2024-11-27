9ee67442099323ff976c59042282f2454f9a1af8
Rhafael Chandra <109317539+rhafaelc@users.noreply.github.com>
2024-11-27 12:20:16 +0700

    change title

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,12 @@
 
 import AuthGuard from "@/components/auth/auth-guard";
 import { useUser } from "@/lib/auth";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DermaScan: Admin Dashboard",
+  description: "DermaScan: Admin Dashboard",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const user = useUser();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  redirect("/login");
-  return <div>Home</div>;
+  return redirect("/login");
 }
